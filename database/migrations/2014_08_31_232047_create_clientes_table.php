63cca5160c0dad7bacb8061b34a130cedba667b3
Lisbeh <lisbethlourdesportilloazurdia9@gmail.com>
2020-10-20 00:12:58 -0600

    Migración tabla cliente

---
+++ database/migrations/2014_08_31_232047_create_clientes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->bigIncrements('idCliente');
+            $table->foreignId('persona_id')->references('idPersona')->on('persona')//Llave foranea tabla puesto
+            ->onUpdate('cascade');//Actualizacion de datos en cascada
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente');
+    }
+}
