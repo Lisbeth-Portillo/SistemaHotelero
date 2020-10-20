ebc2db010166e328d79e3ba465e9503e0181c80d
Lisbeh <lisbethlourdesportilloazurdia9@gmail.com>
2020-10-20 00:27:22 -0600

    Migracion tabla hotel

---
+++ database/migrations/2020_08_24_154023_create_hotels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHotelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotel', function (Blueprint $table) {
+            $table->bigIncrements('idHotel');
+            $table->string('foto', 100);
+            $table->string('nombre', 20);
+            $table->string('direccion', 60);
+            $table->string('email', 25);
+            $table->string('telefono', 10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotel');
+    }
+}
